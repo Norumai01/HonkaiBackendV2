fa1eaa5192b1a96820ea3bf0cec961f4f739570f
Norumai01 <johnnydinhnguyen2001@gmail.com>
2025-03-10 21:06:39 -0400

    Added additional validation

--- src/main/java/com/norumai/honkaiwebsitebackend/dto/LoginRequest.java
+++ src/main/java/com/norumai/honkaiwebsitebackend/dto/LoginRequest.java
@@ -1,14 +1,17 @@
 package com.norumai.honkaiwebsitebackend.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
 
+    @Size(min = 2, max = 320, message = "Username or Email is too long or short.")
     @NotBlank(message = "Username or Email must be provided.")
     private String userInput;
 
+    @Size(min = 5, max = 75, message = "Password is too long or short.")
     @NotBlank(message = "Password must be provided.")
     private String password;
 }
--- src/main/java/com/norumai/honkaiwebsitebackend/dto/RegisterRequest.java
+++ src/main/java/com/norumai/honkaiwebsitebackend/dto/RegisterRequest.java
@@ -2,18 +2,22 @@ package com.norumai.honkaiwebsitebackend.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
 
+    @Size(min = 2, max = 50, message = "Username is too long or short.")
     @NotBlank(message = "Username must be provided.")
     private String username;
 
+    @Size(min = 2, max = 320, message = "Email is too long or short.")
     @Email(message = "Email should be valid.")
     @NotBlank(message = "Email must be provided.")
     private String email;
 
+    @Size(min = 5, max = 75, message = "Password is too long or short.")
     @NotBlank(message = "Password must be provided.")
     private String password;
 
