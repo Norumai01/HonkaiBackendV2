504981c20ad5e1b186b854ee21b6475e30eb1ffc
Norumai01 <johnnydinhnguyen2001@gmail.com>
2025-05-07 04:01:57 -0400

    Added unit tests for mocking user creation and authentication system.

--- src/test/java/com/norumai/honkaiwebsitebackend/HonkaiWebsiteBackendApplicationTests.java
+++ src/test/java/com/norumai/honkaiwebsitebackend/HonkaiWebsiteBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.norumai.honkaiwebsitebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Use the test properties file for testing.
 class HonkaiWebsiteBackendApplicationTests {
 
     @Test
